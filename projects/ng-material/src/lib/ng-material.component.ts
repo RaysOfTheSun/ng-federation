99f4fa697f6bd1f7588b0982e93e79ed4fa7f6da
Carl <rdelsol35@gmail.com>
2022-05-31 20:48:33 +0800

    add initial sources

---
+++ projects/ng-material/src/lib/ng-material.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-ng-material',
+  template: `
+    <p>
+      ng-material works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class NgMaterialComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
